0274abfcd341233ea2c787606debbb096fd9ed7c
cvdat2097 <cvdat2097@gmail.com>
2019-08-15 18:40:49 +0700

    init twitter

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col" >
+                      <button className="btn btn-primary">Hello</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
