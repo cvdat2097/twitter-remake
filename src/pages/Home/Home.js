a1b2e7c7fb48c2b7f7d33ac5d71217c0fd535405
cvdat2097 <cvdat2097@gmail.com>
2019-08-15 21:26:09 +0700

    init color palette

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,13 @@ class Home extends React.Component {
         return (
             <div className="row">
                 <div className="col">
+                    <h1>Twitter Remake</h1>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Doloribus nisi vero quia in dicta illum eos enim
+                        quo harum, magni soluta, debitis magnam. Voluptatem
+                        possimus laudantium quod id, itaque animi.
+                    </p>
                     <Button>Hello</Button>
                 </div>
             </div>
