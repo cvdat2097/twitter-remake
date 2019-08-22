553327d74e9dce522a520bfb1791644d04effec2
cvdat2097 <cvdat2097@gmail.com>
2019-08-22 16:42:35 +0700

    use function component 100%

--- src/components/utils/Popover/Popover.js
+++ src/components/utils/Popover/Popover.js
@@ -3,86 +3,76 @@ import PropTypes from 'prop-types';
 
 import PopoverPosition from './PopoverPosition';
 
-class Popover extends React.Component {
-    constructor(props) {
-        super(props);
+const getStyleFromPosition = position => {
+    switch (position) {
+        case PopoverPosition.TOP_RIGHT:
+            return {
+                top: 0,
+                right: 0,
+            };
 
-        this.handleBackdropClick = this.handleBackdropClick.bind(this);
+        case PopoverPosition.TOP_LEFT:
+            return {
+                top: 0,
+                left: 0,
+            };
+
+        case PopoverPosition.BOTTOM_RIGHT:
+            return {
+                bottom: 0,
+                right: 0,
+            };
+
+        case PopoverPosition.BOTTOM_LEFT:
+            return {
+                bottom: 0,
+                left: 0,
+            };
+
+        default:
+            throw new Error('Invalid Position Type');
     }
+};
 
-    get styleFromPosition() {
-        const { position } = this.props;
+const Popover = props => {
+    const { children, visible, position, onClose } = props;
 
-        switch (position) {
-            case PopoverPosition.TOP_RIGHT:
-                return {
-                    top: 0,
-                    right: 0,
-                };
+    const styleFromPosition = getStyleFromPosition(position);
 
-            case PopoverPosition.TOP_LEFT:
-                return {
-                    top: 0,
-                    left: 0,
-                };
-
-            case PopoverPosition.BOTTOM_RIGHT:
-                return {
-                    bottom: 0,
-                    right: 0,
-                };
-
-            case PopoverPosition.BOTTOM_LEFT:
-                return {
-                    bottom: 0,
-                    left: 0,
-                };
-
-            default:
-                throw new Error('Invalid Position Type');
-        }
-    }
-
-    handleBackdropClick(event) {
+    const handleBackdropClick = event => {
         event.stopPropagation();
-
-        const { onClose } = this.props;
 
         onClose && onClose();
-    }
+    };
 
-    handlePopoverClick(event) {
+    const handlePopoverClick = event => {
         event.stopPropagation();
+    };
+
+    if (visible) {
+        return (
+            <>
+                <div
+                    onClick={handlePopoverClick}
+                    className="d-inline-flex position-absolute shadow bg-white overflow-hidden"
+                    style={{
+                        ...styleFromPosition,
+                        zIndex: 9999,
+                        maxWidth: '75vw',
+                    }}
+                >
+                    {children}
+                </div>
+                <div
+                    onClick={handleBackdropClick}
+                    className="popover-backdrop fixed-top fixed-bottom"
+                />
+            </>
+        );
     }
 
-    render() {
-        const { children, visible } = this.props;
-
-        if (visible) {
-            return (
-                <>
-                    <div
-                        onClick={this.handlePopoverClick}
-                        className="d-inline-flex position-absolute shadow bg-white overflow-hidden"
-                        style={{
-                            ...this.styleFromPosition,
-                            zIndex: 9999,
-                            maxWidth: '75vw',
-                        }}
-                    >
-                        {children}
-                    </div>
-                    <div
-                        onClick={this.handleBackdropClick}
-                        className="popover-backdrop fixed-top fixed-bottom"
-                    />
-                </>
-            );
-        }
-
-        return null;
-    }
-}
+    return null;
+};
 
 Popover.propTypes = {
     position: PropTypes.oneOf([
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,64 +12,58 @@ import FollowSuggestion from '../../components/FollowSuggestion';
 import TweetComposer from '../../components/tweet/TweetComposer';
 import Tweet from '../../components/tweet/Tweet';
 
-import './homepage.scss';
-
 import { TWEETS } from '../../mock/tweet';
 import { TRENDS } from '../../mock/trends';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className="row no-gutters">
-                <div className="col col-sm border-left border-right">
-                    <section id="main">
-                        <div className="tw-header bg-white border-bottom position-sticky fixed-top">
-                            <HomeHeader />
+import './homepage.scss';
+
+const Home = () => {
+    return (
+        <div className="row no-gutters">
+            <div className="col col-sm border-left border-right">
+                <section id="main">
+                    <div className="tw-header bg-white border-bottom position-sticky fixed-top">
+                        <HomeHeader />
+                    </div>
+
+                    <div className="d-flex flex-column">
+                        <div className="d-none d-md-block">
+                            <TweetComposer />
+
+                            <div
+                                className="bg-light d-inline-block w-100"
+                                style={{
+                                    height: 10,
+                                }}
+                            />
                         </div>
 
-                        <div className="d-flex flex-column">
-                            <div className="d-none d-md-block">
-                                <TweetComposer />
-
-                                <div
-                                    className="bg-light d-inline-block w-100"
-                                    style={{
-                                        height: 10,
-                                    }}
-                                />
-                            </div>
-
-                            <div>
-                                {TWEETS.map(({ user, post }, index) => (
-                                    <Tweet
-                                        key={index}
-                                        user={user}
-                                        post={post}
-                                    />
-                                ))}
-                            </div>
+                        <div>
+                            {TWEETS.map(({ user, post }, index) => (
+                                <Tweet key={index} user={user} post={post} />
+                            ))}
                         </div>
-                    </section>
-                </div>
-                <div className="col-md-4 d-none d-md-block">
-                    <section id="suggestion">
-                        <div className="tw-header p-2 bg-white position-sticky fixed-top">
-                            <SearchBar />
-                        </div>
-                        <div className="p-2">
-                            <TrendsList trends={TRENDS} />
-                        </div>
-                        <div className="p-2">
-                            <FollowSuggestion />
-                        </div>
-                    </section>
-
-                    <HomeFooter />
-                </div>
+                    </div>
+                </section>
             </div>
-        );
-    }
-}
+            <div className="col-md-4 d-none d-md-block">
+                <section id="suggestion">
+                    <div className="tw-header p-2 bg-white position-sticky fixed-top">
+                        <SearchBar />
+                    </div>
+                    <div className="p-2">
+                        <TrendsList trends={TRENDS} />
+                    </div>
+                    <div className="p-2">
+                        <FollowSuggestion />
+                    </div>
+                </section>
+
+                <HomeFooter />
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = ({ tweets }) => ({ tweets });
 const mapDispatchToProps = {
